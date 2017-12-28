b4542cba820687e5cfec615d91926823f6c58188
srcibex <srcibex.gmail.com>
2017-12-28 14:21:58 -0800

    Initial Setup

---
+++ src/main/java/org/inutile/springalpha/HelloController.java
@@ -0,0 +1,29 @@
+package org.inutile.springalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloController 
+{
+    @RequestMapping("/")
+    @ResponseBody
+    String home() 
+    {
+        return "Hello World!";
+    }
+    
+//    private void shouldBeRemoved(int unused_arg) 
+//    {
+//    	System.out.println("This is not needed");
+//    }
+
+    public static void main(String[] args) throws Exception 
+    {
+        SpringApplication.run(HelloController.class, args);
+    }
+}
